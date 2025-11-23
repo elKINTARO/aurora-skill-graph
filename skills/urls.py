17b5e9d3f990332173d50d2980fc1309d6ed734f
Kiril bc2025-6 <fedotovpoliyt@gmail.com>
2025-11-24 00:31:47 +0200

    Initial commit - add logic for starting and ending learning skills

--- skills/urls.py
+++ skills/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.skill_list, name='skill_list'),
+    path('skill/<slug:skill_slug>/change/<str:new_status>/', views.change_status, name='change_status'),
 ]
--- skills/views.py
+++ skills/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Skill
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Skill, UserSkillProgress
 
 def skill_list(request):
     skills = Skill.objects.prefetch_related(
@@ -7,4 +8,27 @@ def skill_list(request):
         'required_by__to_skill',
     ).all()
 
-    return render(request, 'skills/skill_list.html', {'skills': skills}
+    if request.user.is_authenticated:
+        user_progress = {
+            p.skill_id: p.status
+            for p in UserSkillProgress.objects.filter(user=request.user)
+        }
+
+        for skill in skills:
+            skill.my_status = user_progress.get(skill.id, 'todo')
+
+    return render(request, 'skills/skill_list.html', {'skills': skills})
+
+@login_required
+def change_status(request, skill_slug, new_status):
+    skill = get_object_or_404(Skill, slug=skill_slug)
+
+    progress, created = UserSkillProgress.objects.get_or_create(
+        user=request.user,
+        skill=skill
+    )
+
+    progress.status = new_status
+    progress.save()
+
+    return redirect('skill_list')
